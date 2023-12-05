9b8037c2618100310c7b75c84259488c9f561ef2
Ahmad Maarif <ahmad.maarif.ft21@mail.umy.ac.id>
2023-12-06 03:30:07 +0700

    add class container app

--- app/src/main/java/com/example/roomsiswa/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/roomsiswa/repositori/ContainerApp.kt
@@ -1,11 +1,21 @@
 package com.example.roomsiswa.repositori
 
 import android.content.Context
+import com.example.roomsiswa.Data.DatabaseSiswa
+import com.example.roomsiswa.Data.SiswaDao
 
 interface ContainerApp {
+    fun OfflineRepositoriSiswa(siswaDao: SiswaDao)
+
     val repositoriSiswa:RepositoriSiswa
 }
 
-class ContainerDataApp(private val context: Context):ContainerApp{
-    
+interface ContainerApp {
+    val repositoriSiswa: RepositoriSiswa
+}
+
+class ContainerDataApp(private val context: Context): ContainerApp{
+    override val repositoriSiswa: RepositoriSiswa by lazy {
+        OfflineRepositoriSiswa(DatabaseSiswa.getDatabase(context).siswaDao())
+    }
 }

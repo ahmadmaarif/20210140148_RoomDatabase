48d10d5d977e38b7a0b48a9128658d1886900ef6
Ahmad Maarif <ahmad.maarif.ft21@mail.umy.ac.id>
2023-12-06 03:33:25 +0700

    add class offlineRepo

--- app/src/main/java/com/example/roomsiswa/repositori/OfflineRepositori.kt
+++ app/src/main/java/com/example/roomsiswa/repositori/OfflineRepositori.kt
@@ -1,6 +1,27 @@
 package com.example.roomsiswa.repositori
 
+import com.example.roomsiswa.Data.Siswa
 import com.example.roomsiswa.Data.SiswaDao
+import java.util.concurrent.Flow
 
-class OfflineRepositori {private val siswaDao:SiswaDao):Repositori
+class OfflineRepositoriSiswa(private val siswaDao: SiswaDao):RepositoriSiswa {
+    override fun getAllSiswaStream(): Flow<List<Siswa>> {
+        return siswaDao.getAllSiswa()
+    }
+
+    override fun getSiswaStream(id: Int): Flow<Siswa?> {
+        return siswaDao.getSiswa(id)
+    }
+
+    override suspend fun insertSiswa(siswa: Siswa) {
+        return siswaDao.insert(siswa)
+    }
+
+    override suspend fun deleteSiswa(siswa: Siswa) {
+        return siswaDao.delete(siswa)
+    }
+
+    override suspend fun updateSiswa(siswa: Siswa) {
+        return siswaDao.update(siswa)
+    }
 }
